7a107f632ab54a6053de2ea64599d2518dd5ac34
flux <95105632+hyzek@users.noreply.github.com>
2023-02-19 00:06:45 +0100

    first release

---
+++ Source/Hacks/Chams.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <array>
+
+#include "../Config.h"
+#include "../Memory.h"
+
+namespace csgo
+{
+
+class Entity;
+struct ModelRenderInfo;
+class matrix3x4;
+class StudioRender;
+
+}
+
+class ClientInterfaces;
+class EngineInterfaces;
+class OtherInterfaces;
+
+class Chams {
+public:
+    Chams(const ClientInterfaces& clientInterfaces, const EngineInterfaces& engineInterfaces, const OtherInterfaces& interfaces, const Memory& memory)
+        : clientInterfaces{ clientInterfaces }, engineInterfaces{ engineInterfaces }, interfaces{ interfaces }, memory{ memory }
+    {
+    }
+
+    bool render(Backtrack& backtrack, Config& config, void*, void*, const csgo::ModelRenderInfo&, csgo::matrix3x4*) noexcept;
+    static void updateInput(Config& config) noexcept;
+private:
+    void renderPlayer(Backtrack& backtrack, Config& config, const csgo::Entity& player) noexcept;
+    void renderWeapons(Config& config) noexcept;
+    void renderHands(Config& config) noexcept;
+    void renderSleeves(Config& config) noexcept;
+
+    bool appliedChams;
+    void* ctx;
+    void* state;
+    const csgo::ModelRenderInfo* info;
+    csgo::matrix3x4* customBoneToWorld;
+
+    void applyChams(const std::array<Config::Chams::Material, 7>& chams, int health = 0, csgo::matrix3x4* customMatrix = nullptr) noexcept;
+    
+    ClientInterfaces clientInterfaces;
+    EngineInterfaces engineInterfaces;
+    OtherInterfaces interfaces;
+    const Memory& memory;
+};
---
+++ Source/Hacks/Features.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Aimbot.h"
+#include "Backtrack.h"
+#include "Chams.h"
+#include "Glow.h"
+#include "Visuals.h"
+#include "Misc.h"
+#include <InventoryChanger/InventoryChanger.h>
+
+#include <Interfaces/OtherInterfaces.h>
+
+struct Features {
+    Aimbot aimbot;
+    Backtrack backtrack;
+    Chams chams;
+    Glow glow;
+    Visuals visuals;
+    inventory_changer::InventoryChanger inventoryChanger;
+    Misc misc;
+};
+
+[[nodiscard]] inline Features createFeatures(const Memory& memory, const ClientInterfaces& clientInterfaces, const EngineInterfaces& engineInterfaces, const OtherInterfaces& otherInterfaces, const PatternFinder& clientPatternFinder, const PatternFinder& enginePatternFinder, Helpers::RandomGenerator& randomGenerator)
+{
+    return Features{
+        .backtrack = Backtrack{ otherInterfaces.getCvar() },
+        .chams{ clientInterfaces, engineInterfaces, otherInterfaces, memory },
+        .glow{ clientPatternFinder },
+        .visuals{ memory, otherInterfaces, clientInterfaces, engineInterfaces, clientPatternFinder, enginePatternFinder },
+        .inventoryChanger{ inventory_changer::createInventoryChanger(engineInterfaces, clientInterfaces, otherInterfaces, memory, clientPatternFinder, randomGenerator) },
+        .misc{ clientInterfaces, engineInterfaces, otherInterfaces, memory, clientPatternFinder, enginePatternFinder }
+    };
+}
